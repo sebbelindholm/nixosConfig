bc4a2573e812c3bd88fa0614df7ae7de318e0d62
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2023-09-27 16:56:51 +0200

    Added thinkpad.

---
+++ host/thinkpad/default.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../../modules
+      ../../configs
+    ];
+/**
+  boot = {
+    #kernelParams = [ "intel_iommu=on" "iommu=pt" ];
+    #initrd.kernelModules = [ "amdgpu" ];
+  };
+
+  services = {
+    xserver = {
+      videoDrivers = [ "amdgpu" ];
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+**/
+  system.stateVersion = "22.05";
+}
