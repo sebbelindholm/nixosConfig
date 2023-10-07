e513b5047793cac06a7a5df69e60806e51984dfb
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2023-10-07 11:00:29 +0200

    Update.

---
+++ configs/dconf/dconf.nix
@@ -0,0 +1,273 @@
+# Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
+{ lib, ... }:
+
+with lib.hm.gvariant;
+
+{
+  dconf.settings = {
+    "org/gnome/Console" = {
+      last-window-size = mkTuple [ 812 577 ];
+    };
+
+    "org/gnome/Totem" = {
+      active-plugins = [ "autoload-subtitles" "variable-rate" "movie-properties" "apple-trailers" "recent" "vimeo" "screensaver" "save-file" "skipto" "rotation" "screenshot" "open-directory" "mpris" ];
+      subtitle-encoding = "UTF-8";
+    };
+
+    "org/gnome/control-center" = {
+      last-panel = "keyboard";
+      window-state = mkTuple [ 980 640 ];
+    };
+
+    "org/gnome/desktop/app-folders" = {
+      folder-children = [ "Utilities" "YaST" ];
+    };
+
+    "org/gnome/desktop/app-folders/folders/Utilities" = {
+      apps = [ "gnome-abrt.desktop" "gnome-system-log.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" "org.gnome.Connections.desktop" "org.gnome.DejaDup.desktop" "org.gnome.Dictionary.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.eog.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.fonts.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.tweaks.desktop" "org.gnome.Usage.desktop" "vinagre.desktop" ];
+      categories = [ "X-GNOME-Utilities" ];
+      name = "X-GNOME-Utilities.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/YaST" = {
+      categories = [ "X-SuSE-YaST" ];
+      name = "suse-yast.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/background" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file:///home/sebastian/.local/share/backgrounds/2023-09-29-20-49-24-b-398.jpg";
+      picture-uri-dark = "file:///home/sebastian/.local/share/backgrounds/2023-09-29-20-49-24-b-398.jpg";
+      primary-color = "#000000000000";
+      secondary-color = "#000000000000";
+    };
+
+    "org/gnome/desktop/calendar" = {
+      show-weekdate = true;
+    };
+
+    "org/gnome/desktop/input-sources" = {
+      sources = [ (mkTuple [ "xkb" "se" ]) ];
+      xkb-options = [ "terminate:ctrl_alt_bksp" ];
+    };
+
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      cursor-theme = "Bibata-Modern-Ice";
+      font-antialiasing = "rgba";
+      font-hinting = "full";
+      gtk-theme = "Orchis-Dark";
+      icon-theme = "Papirus";
+      monospace-font-name = "JetBrainsMono Nerd Font Medium 12";
+      text-scaling-factor = 1.0;
+    };
+
+    "org/gnome/desktop/notifications" = {
+      application-children = [ "org-gnome-nautilus" "org-gnome-baobab" "gnome-power-panel" "code" "org-gnome-console" "firefox" "webcord" "steam" "spotify" ];
+    };
+
+    "org/gnome/desktop/notifications/application/code" = {
+      application-id = "code.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/firefox" = {
+      application-id = "firefox.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/gnome-power-panel" = {
+      application-id = "gnome-power-panel.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/minecraft-launcher" = {
+      application-id = "minecraft-launcher.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-baobab" = {
+      application-id = "org.gnome.baobab.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-console" = {
+      application-id = "org.gnome.Console.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-nautilus" = {
+      application-id = "org.gnome.Nautilus.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-settings" = {
+      application-id = "org.gnome.Settings.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-prismlauncher-prismlauncher" = {
+      application-id = "org.prismlauncher.PrismLauncher.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/spotify" = {
+      application-id = "spotify.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/steam" = {
+      application-id = "steam.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/webcord" = {
+      application-id = "webcord.desktop";
+    };
+
+    "org/gnome/desktop/peripherals/mouse" = {
+      accel-profile = "flat";
+      speed = 0.0;
+    };
+
+    "org/gnome/desktop/peripherals/touchpad" = {
+      two-finger-scrolling-enabled = true;
+    };
+
+    "org/gnome/desktop/screensaver" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file:///home/sebastian/.local/share/backgrounds/2023-09-29-20-49-24-b-398.jpg";
+      primary-color = "#000000000000";
+      secondary-color = "#000000000000";
+    };
+
+    "org/gnome/desktop/search-providers" = {
+      sort-order = [ "org.gnome.Contacts.desktop" "org.gnome.Documents.desktop" "org.gnome.Nautilus.desktop" ];
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-input-source = [];
+      switch-input-source-backward = [];
+      switch-to-workspace-left = [ "<Super>j" ];
+      switch-to-workspace-right = [ "<Super>odiaeresis" ];
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = "appmenu:minimize,maximize,close";
+      resize-with-right-button = true;
+    };
+
+    "org/gnome/evolution-data-server" = {
+      migrated = true;
+    };
+
+    "org/gnome/file-roller/listing" = {
+      list-mode = "as-folder";
+      name-column-width = 250;
+      show-path = false;
+      sort-method = "name";
+      sort-type = "ascending";
+    };
+
+    "org/gnome/file-roller/ui" = {
+      sidebar-width = 200;
+      window-height = 480;
+      window-width = 600;
+    };
+
+    "org/gnome/mutter" = {
+      dynamic-workspaces = false;
+      edge-tiling = true;
+      workspaces-only-on-primary = true;
+    };
+
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "icon-view";
+      migrated-gtk-settings = true;
+      search-filter-time-type = "last_modified";
+      search-view = "list-view";
+    };
+
+    "org/gnome/nautilus/window-state" = {
+      initial-size = mkTuple [ 890 550 ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      search = [];
+    };
+
+    "org/gnome/shell" = {
+      app-picker-layout = "[{'org.gnome.Geary.desktop': <{'position': <0>}>, 'org.gnome.Contacts.desktop': <{'position': <1>}>, 'org.gnome.Weather.desktop': <{'position': <2>}>, 'org.gnome.clocks.desktop': <{'position': <3>}>, 'org.gnome.Maps.desktop': <{'position': <4>}>, 'org.gnome.Calendar.desktop': <{'position': <5>}>, 'org.gnome.Photos.desktop': <{'position': <6>}>, 'org.gnome.Totem.desktop': <{'position': <7>}>, 'org.gnome.Calculator.desktop': <{'position': <8>}>, 'ca.desrt.dconf-editor.desktop': <{'position': <9>}>, 'simple-scan.desktop': <{'position': <10>}>, 'discord.desktop': <{'position': <11>}>, 'gnome-system-monitor.desktop': <{'position': <12>}>, 'org.gnome.Extensions.desktop': <{'position': <13>}>, 'F1 2021.desktop': <{'position': <14>}>, 'Utilities': <{'position': <15>}>, 'geany.desktop': <{'position': <16>}>, 'yelp.desktop': <{'position': <17>}>, 'google-chrome.desktop': <{'position': <18>}>, 'org.gnome.Cheese.desktop': <{'position': <19>}>, 'htop.desktop': <{'position': <20>}>, 'kitty.desktop': <{'position': <21>}>, 'org.gnome.Music.desktop': <{'position': <22>}>}, {'nvim.desktop': <{'position': <0>}>, 'nixos-manual.desktop': <{'position': <1>}>, 'nvtop.desktop': <{'position': <2>}>, 'org.berarma.Oversteer.desktop': <{'position': <3>}>, 'plank.desktop': <{'position': <4>}>, 'org.prismlauncher.PrismLauncher.desktop': <{'position': <5>}>, 'Proton Experimental.desktop': <{'position': <6>}>, 'pavucontrol.desktop': <{'position': <7>}>, 'spotify.desktop': <{'position': <8>}>, 'org.gnome.TextEditor.desktop': <{'position': <9>}>, 'org.gnome.Tour.desktop': <{'position': <10>}>, 'org.gnome.Epiphany.desktop': <{'position': <11>}>, 'xterm.desktop': <{'position': <12>}>}]";
+      disabled-extensions = [ "native-window-placement@gnome-shell-extensions.gcampax.github.com" ];
+      enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" "unblank@sun.wxg@gmail.com" "user-theme@gnome-shell-extensions.gcampax.github.com" "dash-to-dock@micxgx.gmail.com" "blur-my-shell@aunetx" "widgets@aylur" "impatience@gfxmonk.net" ];
+      favorite-apps = [ "org.gnome.Nautilus.desktop" "code.desktop" "firefox.desktop" "webcord.desktop" "steam.desktop" "org.gnome.Console.desktop" "org.gnome.Settings.desktop" ];
+      last-selected-power-profile = "performance";
+      welcome-dialog-last-shown-version = "44.2";
+    };
+
+    "org/gnome/shell/extensions/aylurs-widgets" = {
+      dash-links-names = [ "reddit" "youtube" "gmail" "twitter" "github" ];
+      dash-links-urls = [ "https://www.reddit.com/" "https://www.youtube.com/" "https://www.gmail.com/" "https://twitter.com/" "https://www.github.com/" ];
+    };
+
+    "org/gnome/shell/extensions/blur-my-shell" = {
+      sigma = 167;
+    };
+
+    "org/gnome/shell/extensions/blur-my-shell/panel" = {
+      blur = false;
+      static-blur = false;
+    };
+
+    "org/gnome/shell/extensions/dash-to-dock" = {
+      apply-custom-theme = false;
+      background-opacity = 0.8;
+      click-action = "focus-minimize-or-previews";
+      dash-max-icon-size = 48;
+      dock-position = "BOTTOM";
+      height-fraction = 0.9;
+      preferred-monitor = -2;
+      preferred-monitor-by-connector = "DP-1";
+      preview-size-scale = 0.0;
+      scroll-action = "switch-workspace";
+      show-mounts = false;
+      show-mounts-only-mounted = false;
+    };
+
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "Orchis-Dark";
+    };
+
+    "org/gnome/shell/world-clocks" = {
+      locations = "@av []";
+    };
+
+    "org/gnome/tweaks" = {
+      show-extensions-notice = false;
+    };
+
+    "org/gtk/gtk4/settings/file-chooser" = {
+      date-format = "regular";
+      location-mode = "path-bar";
+      show-hidden = true;
+      show-size-column = true;
+      show-type-column = true;
+      sidebar-width = 140;
+      sort-column = "name";
+      sort-directories-first = false;
+      sort-order = "ascending";
+      type-format = "category";
+      view-type = "list";
+      window-size = mkTuple [ 859 327 ];
+    };
+
+    "org/gtk/settings/file-chooser" = {
+      date-format = "regular";
+      location-mode = "path-bar";
+      show-hidden = true;
+      show-size-column = true;
+      show-type-column = true;
+      sidebar-width = 165;
+      sort-column = "name";
+      sort-directories-first = false;
+      sort-order = "ascending";
+      type-format = "category";
+      window-position = mkTuple [ 3038 264 ];
+      window-size = mkTuple [ 1203 902 ];
+    };
+
+  };
+}
