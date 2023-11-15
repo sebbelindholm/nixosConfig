a862f4c26c63f1e7d037e7ecd87dc9bb00e25667
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2023-11-15 22:00:53 +0100

    Update.

--- Scripts/hyprScreenConfigurator.sh
+++ Scripts/hyprScreenConfigurator.sh
@@ -2,8 +2,8 @@
 
 case $HOSTNAME in
   nixos-desktop) 
-    hyprctl keyword monitor DP-1, 3440x1440@100, 0x0, 1.0
-    hyprctl keyword monitor HDMI-A-1, 1920x1080@144, 760x1440, 1.0
+    hyprctl keyword monitor DP-1, 3440x1440@100, 0x1080, 1.0
+    hyprctl keyword monitor HDMI-A-1, 1920x1080@144, 760x0, 1.0
     ;;
 
   nixos-thinkpad) 
